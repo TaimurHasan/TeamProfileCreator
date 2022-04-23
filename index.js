bcda7961161df5dc0cc0d050927428a3d3ce3359
TaimurHasan <taimurhasan11@gmail.com>
2022-04-23 10:31:37 -0400

    added inquirer prompts and questions required

---
+++ index.js
@@ -0,0 +1,197 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+// question array for initial prompt on team manager
+const promptManager = () => {
+return inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter the team manager's name.",
+            validate: (nameInput) => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the team manager's name!")
+                    return false;
+                }
+            } 
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Enter the team manager's Employee ID.",
+            validate: (nameInput) => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the team manager's Employee ID!")
+                    return false;
+                }
+            } 
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Enter the team manager's Email.",
+            validate: (nameInput) => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the team manager's Email!")
+                    return false;
+                }
+            } 
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Enter the team manager's office number.",
+            validate: (nameInput) => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the team manager's office number!")
+                    return false;
+                }
+            } 
+        }
+    ])
+};
+
+const promptForTeam = (teamData) => {
+    if(!teamData.employees) {
+        teamData.employees = [];
+    }
+    return inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'employee',
+                message: 'Please select an option for your team:',
+                choices: ['Engineer', 'Intern', 'Finish building my team']
+            },
+            {
+                type: 'input',
+                name: 'name',
+                message: "Please enter this employee's name: ",
+                when: ({ employee }) => {
+                    if(employee !== 'Finish building my team') {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                },
+                validate: (nameInput) => {
+                    if(nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter the employee's name!")
+                        return false;
+                    }
+                } 
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Please enter this employee's id: ",
+                when: ({ employee }) => {
+                    if(employee !== 'Finish building my team') {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                },
+                validate: (nameInput) => {
+                    if(nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter the employee's id!")
+                        return false;
+                    }
+                } 
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Please enter this employee's Email: ",
+                when: ({ employee }) => {
+                    if(employee !== 'Finish building my team') {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                },
+                validate: (nameInput) => {
+                    if(nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter the employee's Email!")
+                        return false;
+                    }
+                } 
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "Please enter this engineer's Github username: ",
+                when: ({ employee }) => {
+                    if(employee === 'Engineer') {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                },
+                validate: (nameInput) => {
+                    if(nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter the engineer's Github username!")
+                        return false;
+                    }
+                } 
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "Please enter this intern's school name: ",
+                when: ({ employee }) => {
+                    if(employee === 'Intern') {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                },
+                validate: (nameInput) => {
+                    if(nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter the intern's school name!")
+                        return false;
+                    }
+                } 
+            }
+        ])
+        .then(employeeData => {
+            if(employeeData.employee !== 'Finish building my team') {
+                teamData.employees.push(employeeData);
+                return promptForTeam(teamData);
+            } else {
+                return teamData;
+            }
+        })
+}
+
+promptManager()
+.then(promptForTeam)
+.then((data) => {
+    console.log(data);
+    const { name, id, email, officeNumber } = data;
+    const manager = new Manager(name, id, email, officeNumber);
+    const role = manager.getRole();
+    console.log(manager, role);
+}) 
